69ccfb3ed4453503d523ed52f04615ab8caa936f
Kevin Dorosh <kcdorosh@gmail.com>
2025-03-10 14:27:01 -0400

    make generate

--- pkg/io/applicator.go
+++ pkg/io/applicator.go
@@ -5,12 +5,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"reflect"
+
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	"reflect"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/structured-merge-diff/v4/fieldpath"
 
