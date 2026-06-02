a4f6ad3171ee7af58fa92e2abd6abc2dac812fff
Kevin Dorosh <kcdorosh@gmail.com>
2026-06-02 16:57:57 -0400

    Simpler fix fix

--- pkg/io/applicator.go
+++ pkg/io/applicator.go
@@ -99,22 +99,24 @@ func (a *APIApplicator) Apply(ctx context.Context, current client.Object, opts .
 		return errors.New("cannot access object metadata")
 	}
 
-	desired := current.DeepCopyObject().(client.Object)
+	ssaDesired := current.DeepCopyObject().(client.Object)
+	// apply options to ssaDesired; we do this to mutate
+	// requestOpts so we can properly evaluate requestOpts.ServerSideApply
+	if err := applyOpts(ctx, ssaDesired, requestOpts, opts); err != nil {
+		return fmt.Errorf("applying options: %w", err)
+	}
 
 	// if server-side apply is enabled, we should also use it to create the object.
 	// We also bypass the below get + diff loop, meaning that even if an apply doesn't change an object it will update the fieldManagers.
 	if requestOpts.ServerSideApply {
-		// apply options to desired
-		if err := applyOpts(ctx, desired, requestOpts, opts); err != nil {
-			return fmt.Errorf("applying options: %w", err)
-		}
-		return a.serverSideApply(ctx, desired, requestOpts)
+		return a.serverSideApply(ctx, ssaDesired, requestOpts)
 	}
 
 	if m.GetName() == "" && m.GetGenerateName() != "" {
 		return a.createNewObject(ctx, current, requestOpts, opts)
 	}
 
+	desired := current.DeepCopyObject().(client.Object)
 	err := a.client.Get(ctx, types.NamespacedName{Name: m.GetName(), Namespace: m.GetNamespace()}, current)
 	if kerrors.IsNotFound(err) {
 		return a.createNewObject(ctx, current, requestOpts, opts)
